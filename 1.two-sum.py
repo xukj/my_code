ca1fdd828a582312f19dafe5ec446950ec956324
Kunjie Xu <kunjiexu@Kunjies-MacBook-Pro.local>
2020-11-30 16:52:50 -0800

    frist commit

---
+++ 1.two-sum.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=1 lang=python
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i, num in enumerate(nums):
+            rest = target - num
+            if rest in dict:
+                return [i, dict[rest]]
+            else:
+                dict[num] = i
+        
+# @lc code=end
+
